a6f42a133fea486d3072bf49ea86f589025c3a1a
Cody_G <cody.gonzalez07@gmail.com>
2017-08-19 16:17:11 -0500

    anotherpush

--- 06week/towersOfHanoi/script.js
+++ 06week/towersOfHanoi/script.js
@@ -2,4 +2,29 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   // Your code here
+  let moved = {};
+
+  document.querySelectorAll('[data-block]').forEach((block) => {
+    block.addEventListener('click', (e) => {
+      e.stopPropagation();
+      moved = { size: e.target.attributes[0].value };
+      e.target.parentNode.removeChild(e.target);
+      console.log(moved.size);
+    });
+  });
+  document.querySelector('[data-stack]').addEventListener('click', (e) => {
+    if (!e.target.children.length) {
+      e.target.appendChild(moved.target);
+    } else {
+      moved.parent.appendChild(moved.target);
+    }
+  });
 });
+// document.querySelector('[data-stack]').addEventListener('click', (e) => {
+//   e.target.appendChild(moved.target);
+// });
+/*
+// moved = { target: e.target, parent: e.target.parentNode };
+// originally at line 10: moved = { size: e.target.attributes[0].value };
+add attributes for each block for the size so as not to be able to stack on each other
+*/
